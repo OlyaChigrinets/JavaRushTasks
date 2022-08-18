31658f659f42039f8ba62051a00869c363653039
OlyaChigrinets <helga.chigrinets@gmail.com>
2022-08-18 14:43:14 +0300

    add solution from JavaSyntax task 0219

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0219/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task02.task0219;
+
+/* 
+Баги робокопа
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int id = 1;
+        int age = 13;
+        String documents = "passport";
+        String behavior = "badass";
+        //напишите тут ваш код
+    }
+}
