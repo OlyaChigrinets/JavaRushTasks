c150b3a7030c452bcce9cff5300fc67ce0b197db
OlyaChigrinets <helga.chigrinets@gmail.com>
2022-08-18 15:27:10 +0300

    add solution from JavaSyntax task 0206

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,10 @@
+package com.javarush.task.pro.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String word = "слово", phrase = "я люблю маму", line = "строка", text = "текст";
+        //напишите тут ваш код
+
+    }
+}
