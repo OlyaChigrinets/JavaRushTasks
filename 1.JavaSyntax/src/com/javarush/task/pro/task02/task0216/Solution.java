c2cf8a073fe49fbb5ece0a74f6ddf277544cb871
OlyaChigrinets <helga.chigrinets@gmail.com>
2022-08-18 15:23:28 +0300

    add solution from JavaSyntax task 0216

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0216/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task02.task0216;
+
+/* 
+Генератор позитивных новостей
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String name= "Olya";
+        String date= "22";
+        String time= "18:00";
+        String adjective= "pow";
+        String noun = "56";
+        String verb = "map";
+
+        //напишите тут ваш код
+
+        System.out.println("Новости:");
+        System.out.println(name + " " + date + " " + time + " изобрел " + adjective + " " + noun + " который " + verb + " террабайты данных в минуту.");
+    }
+}
