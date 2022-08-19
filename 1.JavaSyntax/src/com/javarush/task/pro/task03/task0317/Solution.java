c36f76aac992fdf1c08232effc9095b33a449272
OlyaChigrinets <helga.chigrinets@gmail.com>
2022-08-19 14:32:07 +0300

    add solution from JavaSyntax task 0317

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0317/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.pro.task03.task0317;
+
+import java.util.Scanner;
+
+/* 
+Пищевой конвеер
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int numb1 = s. nextInt();
+        int numb2 = s. nextInt();
+        int numb3 = s. nextInt();
+        int numb4 = s. nextInt();
+        boolean isnumb1 = (numb1>0);
+        if (isnumb1)
+            System.out.println(numb1);
+        boolean isnumb2 = (numb2>0);
+        if (isnumb2)
+            System.out.println(numb2);
+        boolean isnumb3 = (numb3>0);
+        if (isnumb3)
+            System.out.println(numb3);
+        boolean isnumb4 = (numb4>0);
+        if (isnumb4)
+            System.out.println(numb4);
+
+        //напишите тут ваш код
+    }
+}
