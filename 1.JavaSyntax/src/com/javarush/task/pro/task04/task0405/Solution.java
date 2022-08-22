bcc9552cb568754f8287226d097ccc0ad9e8a500
OlyaChigrinets <helga.chigrinets@gmail.com>
2022-08-22 16:39:07 +0300

    add solution from JavaSyntax task 0405

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0405/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task04.task0405;
+
+/* 
+Незаполненный прямоугольник
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int x = 0;
+        while (x < 10) {
+            int y = 0;
+            while (y < 20) {
+                if (x == 0 || x == 9) System.out.print("Б");
+                else if (y == 0 || y == 19) System.out.print("Б");
+                else System.out.print(" ");
+                y++;
+            }
+            x++;
+            System.out.println();
+        }
+    }
+}
