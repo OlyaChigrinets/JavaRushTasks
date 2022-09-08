2e31c685b3bf0f5d596ae6e4e5ec32bc81e369fc
OlyaChigrinets <helga.chigrinets@gmail.com>
2022-09-08 19:03:05 +0300

    add solution from JavaSyntax task0529(3)

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0529/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0529/Solution.java
@@ -13,12 +13,21 @@ public class Solution {
     public static int width = 30;
     public static int height = 10;
     public static String[][] field = new String[height][width];
+    public static int [][] bombs = new int[height][width];
 
     public static void main(String[] args) {
+        for (int i = 0; i < bombs.length; i++) {
+            Arrays.fill(bombs[i],0);
+            Arrays.fill(bombs[i],4,14,1);
+        }
 
+        
         for (int i = 0; i < field.length; i++) {
             Arrays.fill(field[i], empty);
-            Arrays.fill(field[i],3,4,robotank);
+        }
+        for (int i = 0; i < field.length; i++) {
+            int j = (int) (Math.random()*width);
+            field[i][j] = robotank;
         }
         for (int i = 0; i < field.length; i++) {
             for (int j = 0; j < field[i].length; j++) {
