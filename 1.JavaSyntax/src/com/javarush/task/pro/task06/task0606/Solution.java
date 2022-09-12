db583d89477c0ba3d614dfe31929825017c47b5c
OlyaChigrinets <helga.chigrinets@gmail.com>
2022-09-12 17:31:24 +0300

    add solution from JavaSyntax task0606

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,44 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(){
+
+    }
+
+    public static void universalMethod(int a){
+
+    }
+    public static void universalMethod(String str){
+
+    }
+    public static void universalMethod(int[] a){
+
+    }
+    public static void universalMethod(int[][] data){
+
+    }
+    public static void universalMethod(double d){
+
+    }
+    public static void universalMethod(String[] strings){
+
+    }
+    public static void universalMethod(int a, int b){
+
+    }
+    public static void universalMethod(int a, String text){
+
+    }
+    public static void universalMethod(int [][][] a){
+
+    }
+}
