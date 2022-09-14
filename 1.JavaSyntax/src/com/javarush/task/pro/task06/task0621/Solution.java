5663250eeca3715c9bb17201b80f9e928af4608a
OlyaChigrinets <helga.chigrinets@gmail.com>
2022-09-14 18:43:09 +0300

    add solution from JavaSyntax task0612

--- 1.JavaSyntax/src/com/javarush/task/pro/task06/task0621/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0621/Solution.java
@@ -7,7 +7,6 @@ package com.javarush.task.pro.task06.task0621;
 public class Solution {
     public static String demons = "демоны";
     public static void main(String[] args) {
-
         if ("демоны".equals(demons)) {
             System.out.println("На друзей напали демоны.");
         }
