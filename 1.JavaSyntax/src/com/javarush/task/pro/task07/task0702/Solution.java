ba4d26dfc266f82979c636f637bc3278e3e343a3
OlyaChigrinets <helga.chigrinets@gmail.com>
2022-09-17 16:51:52 +0300

    add solution from JavaSyntax task0702

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+    int a = Integer.MAX_VALUE;
+    short b = Short.MAX_VALUE;
+    byte c = Byte.MAX_VALUE;
+    long q = Long.MAX_VALUE;
+
+}
