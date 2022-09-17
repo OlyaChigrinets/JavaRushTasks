5de30fbdb6e9e6de46d96c5bd6e6803e64e98dae
OlyaChigrinets <helga.chigrinets@gmail.com>
2022-09-17 16:40:13 +0300

    add solution from JavaSyntax task0701

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    byte a = 12;
+    int b = 12;
+    short c = 12;
+    long d = 12;
+    boolean e = true;
+    char w = 12;
+    float o = 12;
+    double l = 12.3;
+}
