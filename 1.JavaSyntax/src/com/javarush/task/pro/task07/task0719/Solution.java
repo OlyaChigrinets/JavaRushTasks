3ba7c271098bc286d60af49cee0e5c8f85192d84
OlyaChigrinets <helga.chigrinets@gmail.com>
2022-09-18 17:18:36 +0300

    add solution from JavaSyntax task0719

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0719/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0719;
+
+/* 
+Космическая Робосутра
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Gun x = new Gun();
+        RepairDrone a = new RepairDrone();
+        RobotCleaner b = new RobotCleaner();
+        AutomaticKitchen c = new AutomaticKitchen();
+        ProtectiveFieldGenerator v = new ProtectiveFieldGenerator();
+    }
+}
