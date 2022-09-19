554ce037e5f918c8eaf63c74d8dba611f6882271
OlyaChigrinets <helga.chigrinets@gmail.com>
2022-09-19 16:15:21 +0300

    add solution from JavaSyntax task0720

--- 1.JavaSyntax/src/com/javarush/task/pro/task07/task0720/Nimrod.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0720/Nimrod.java
@@ -4,6 +4,7 @@ public class Nimrod {
     public static int laser = 100_000;
     public static int rocket = 10_000;
     public static int angelicaKiss;
+    
     public int health = 1_000_000;
 
     public void defend(int b) {
