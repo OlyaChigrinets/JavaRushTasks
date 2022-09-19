313d26e0cee6324c1ce1a3394cc461abfc1a7939
OlyaChigrinets <helga.chigrinets@gmail.com>
2022-09-19 15:21:47 +0300

    add solution from JavaSyntax task0720

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0720/Nimrod.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0720;
+
+public class Nimrod {
+    public static int laser = 100_000;
+    public static int rocket = 10_000;
+    public static int angelicaKiss;
+    public int health = 1_000_000;
+
+    public void defend(int b) {
+        health = health - b;
+    }
+    public int attack() {return rocket;}
+}
