61e72941c37be8eb4068cfb6f511e9f91a75d73b
OlyaChigrinets <helga.chigrinets@gmail.com>
2022-10-12 15:56:28 +0300

    add solution from JavaSyntax task1024

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1024/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task10.task1024;
+
+/* 
+Ужасный Двубитный
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {new TerribleTwoBit();
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1024/TerribleTwoBit.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task10.task1024;
+
+public class TerribleTwoBit {
+    private String mission = "разрушать и разорять";
+
+    public TerribleTwoBit() {
+        this.mission = "быстрые и комфортные перевозки";
+        System.out.println("Моя миссия: " + mission);
+    }
+
+
+    // public String getMission() {
+      //  return "Моя миссия: " + mission;
+    //}
+}
