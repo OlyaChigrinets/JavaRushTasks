c2987767c5c0c41e057b5d039d1cda941e260976
OlyaChigrinets <helga.chigrinets@gmail.com>
2022-10-27 22:45:05 +0300

    add solution from JavaSyntax task1207

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1207/Animal.java
@@ -0,0 +1,7 @@
+package com.javarush.task.pro.task12.task1207;
+
+public class Animal {
+    public void eat() {
+        System.out.println("Animal is eating...");
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1207/Test.java
@@ -0,0 +1,10 @@
+package com.javarush.task.pro.task12.task1207;
+
+public class Test {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.eat();
+        Animal animal1 = new Dog();
+        animal1.eat();
+    }
+}
