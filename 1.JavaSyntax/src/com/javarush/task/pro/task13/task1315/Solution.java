7ec3590af237e659a6bf920b2edd5cd6ebc55ad2
OlyaChigrinets <helga.chigrinets@gmail.com>
2022-11-09 19:14:30 +0300

    add solution from JavaSyntax task1315

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1315/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task13.task1315;
+
+/* 
+Четыре сезона
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Season one = Season.WINTER;
+        System.out.println(one);
+        Season two = Season.SPRING;
+        System.out.println(two);
+        Season third = Season.SUMMER;
+        System.out.println(third);
+        Season four = Season.AUTUMN;
+        System.out.println(four);
+    }
+}
