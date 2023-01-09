eeec2d32c03b0756db0d8621d706d8377b0472fa
OlyaChigrinets <helga.chigrinets@gmail.com>
2023-01-09 19:24:05 +0300

    add solution from JavaSyntax mini02

--- 8.Games/src/com/javarush/games/minigames/mini01/FirstGame.java
+++ 8.Games/src/com/javarush/games/minigames/mini01/FirstGame.java
@@ -3,9 +3,6 @@ package com.javarush.games.minigames.mini01;
 import com.javarush.engine.cell.Game;
 
 public class FirstGame extends Game {
-    public static void main(String[] args) {
-
-    }
     @Override
     public void initialize() {
     }
