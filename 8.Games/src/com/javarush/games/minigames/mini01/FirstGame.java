d32ce5fc92a589f72147212ee1dccac34c8817ec
OlyaChigrinets <helga.chigrinets@gmail.com>
2023-01-09 16:52:41 +0300

    add solution from JavaSyntax mini01

---
+++ 8.Games/src/com/javarush/games/minigames/mini01/FirstGame.java
@@ -0,0 +1,12 @@
+package com.javarush.games.minigames.mini01;
+
+import com.javarush.engine.cell.Game;
+
+public class FirstGame extends Game {
+    public static void main(String[] args) {
+
+    }
+    @Override
+    public void initialize() {
+    }
+}
