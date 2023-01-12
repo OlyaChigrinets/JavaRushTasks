a9a3e6d4b51aded92026ffc6edd65ebd57b2203c
OlyaChigrinets <helga.chigrinets@gmail.com>
2023-01-12 12:06:31 +0300

    add solution from JavaCore task1207

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(1);
+        Integer e = 34567;
+        print(e);
+    }
+
+    public static void print(int numb) {
+        System.out.println(numb);
+    }
+
+    public static void print(Integer a) {
+        System.out.println(a);
+    }
+}
