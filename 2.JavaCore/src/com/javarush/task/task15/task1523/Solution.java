9ff88fe3fd1fd6789cc09685d5d74674349d6de4
OlyaChigrinets <helga.chigrinets@gmail.com>
2023-01-28 17:15:41 +0300

    add solution from JavaCore task1523

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+        public Solution(short e) {
+        }
+    private Solution(int y) {
+        }
+    protected Solution(String f) {
+        }
+        Solution() {
+        }
+
+}
+
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution {
+    public SubSolution(short e) {
+        super(e);
+    }
+
+    protected SubSolution(String f) {
+        super(f);
+    }
+
+    SubSolution() {
+    }
+}
