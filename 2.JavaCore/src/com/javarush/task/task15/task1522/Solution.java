b10446805ac4ee143c2d9c3aac2ac06965ea64e5
OlyaChigrinets <helga.chigrinets@gmail.com>
2023-01-30 13:59:35 +0300

    add solution from JavaCore task1522

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1522/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task15.task1522;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/* 
+Закрепляем паттерн Singleton
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    static {
+        readKeyFromConsoleAndInitPlanet();
+    }
+
+    public static Planet thePlanet;
+
+
+    public static void readKeyFromConsoleAndInitPlanet() {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        if (s.equals(Planet.SUN)) {
+            thePlanet = Sun.getInstance();
+        } else if (s.equals(Planet.MOON)) {
+            thePlanet = Moon.getInstance();
+        } else if (s.equals(Planet.EARTH)) {
+            thePlanet = Earth.getInstance();
+        } else {
+            thePlanet = null;
+        }
+    }
+}
